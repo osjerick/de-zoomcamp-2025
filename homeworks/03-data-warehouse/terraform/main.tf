f6bf0120177a1c465546e5c2b4a449faa3c59340
Jerick Órdenes <os.jerick@gmail.com>
2025-02-12 14:42:24 -0500

    Terraform.

---
+++ homeworks/03-data-warehouse/terraform/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.20.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = "us-central1"
+}
+
+resource "google_storage_bucket" "data-lake-bucket" {
+  name                        = "taxi-data-${var.common_id}"
+  location                    = "US"
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = false
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 7 // days
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  force_destroy = true
+}
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = "taxi_data_${replace(var.common_id, "-", "_")}"
+}
---
+++ homeworks/03-data-warehouse/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "Project ID"
+  type        = string
+  default     = "< your project ID >"
+}
+
+variable "common_id" {
+  description = "Common ID for all resources"
+  type        = string
+  default     = "< your common ID >"
+}
